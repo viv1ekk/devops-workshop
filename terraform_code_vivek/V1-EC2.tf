bb254cbc73b8612a97fae57a605e13d83e8f7c97
vivek <kvivek.1995@gmail.com>
2025-11-15 19:54:20 +0530

    new commit

--- terraform_code_vivek/V1-EC2.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    region = "us-east-1" 
-}
-
-resource "aws_instance" "demo_server" {
-    ami = "ami-0cae6d6fe6048ca2c"
-    instance_type = "t2.micro"
-    key_name = "dpp" 
-}
